c9e1e0ceacd2c668538713371b82ba52f9cc8fc9
Michael Curry <mdcurry1138@gmail.com>
2019-09-18 19:09:59 -0400

    This version works... for now

--- twitsoff/app.py
+++ twitsoff/app.py
@@ -2,6 +2,7 @@
 from decouple import config
 from flask import Flask, render_template, request
 from .models import DB, User
+from .twitter import add_or_update_user
 
 def create_app():
     """create and config an instance of the Flask App"""
@@ -24,5 +25,29 @@ def create_app():
     def root():
         users = User.query.all()
         return render_template('base.html', title = 'Home', users=users)
+
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None):
+        message = ''
+        # import pdb; pdb.set_trace()
+        name = name or request.values['user_name']
+        try:
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = 'User {} successfully added!'.format(name)
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = 'Error adding {}: {}'.format(name, e)
+            tweets = []
+        return render_template('user.html', title=name, tweets=tweets,
+                                message=message)
+
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title='DB Reset!', users=[])
+
 
     return app
